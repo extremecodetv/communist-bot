b300658b7e278d5ad8707f506e62e452a89f1fa2
kreuwont <icdg@yandex.ru>
2018-11-02 10:25:29 +0300

    Can fix message in rules

--- src/commands/rules.command.js
+++ src/commands/rules.command.js
@@ -1,11 +1,37 @@
-const { bot, rules } = require('../config')
+const { bot } = require('../config')
+const FileStream = require('fs')
 
 const command = '/rules'
+const setCommand = '/set_rules'
+
+let admins = []
+
+bot.getChatAdministrators('@efefewfwevf').then(result => {
+  admins = result
+})
+
 
 module.exports = async (msg) => {
   const isCommand = ~msg.text.indexOf(command)
+  const isSetCommand = ~msg.text.indexOf(setCommand)
+  const isAdmin = admins.find(a => a.user.id === msg.from.id)
 
   if (isCommand) {
-    await bot.sendMessage(msg.chat.id, rules)
+    FileStream.readFile(`${__dirname}/../data/rules.txt`, 'utf8', async (error, rules) => {
+      if (error) throw error;
+      await bot.sendMessage(msg.chat.id, rules)
+    })
+  }
+  else if (isSetCommand && isAdmin) {
+    if (!msg.reply_to_message) {
+      return await bot.sendMessage(msg.chat.id, 'Товарищ! Необходимо выбрать запись для закрепления в качестве правил чата!')
+    }
+
+    FileStream.writeFile(`${__dirname}/../data/rules.txt`, msg.reply_to_message.text, 'utf8', async (error) => {
+      if (error) {
+        await bot.sendMessage(msg.chat.id, 'Произошла ошибка, не удалось установить новые правила')
+        throw error;
+      }
+    })
   }
 }
--- src/config/index.js
+++ src/config/index.js
@@ -1,10 +1,8 @@
-const FileStream = require("fs")
 require('dotenv').config()
 
 module.exports = {
   bot: require('./bot'),
   mongo: {
     uri: process.env.MONGO_URI
-  },
-  rules: FileStream.readFileSync(`${__dirname}/../data/rules.txt`, 'utf8')
+  }
 }
